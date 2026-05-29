681f5ff6cb633f8595dab91fb0fd30d35ddfbd80
bry4ng4rrix <bryanmfb4@gmail.com>
2026-05-29 09:12:28 +0300

    add components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Plus_Jakarta_Sans, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -71,7 +74,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr" className={jakarta.variable}>
+    <html lang="fr" className={cn("font-sans", geist.variable)}>
       <body className="antialiased">{children}</body>
     </html>
   );
